5418320d7cdbd3cb57625c1355a36bd63a64805e
IKEP <34697515+creativeIKEP@users.noreply.github.com>
2020-09-05 21:46:23 +0900

    create start up run application

--- EyeBreakTimer_Windows/main.cpp
+++ EyeBreakTimer_Windows/main.cpp
@@ -26,6 +26,7 @@ HWND resetCheckBoxId;
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 void AppicationInit(HWND hWnd);
+BOOL RegisterMyProgramForStartup(PCWSTR pszAppName, PCWSTR pathToExe, PCWSTR args);
 
 
 int CALLBACK WinMain(
@@ -93,9 +94,6 @@ int CALLBACK WinMain(
 
         return 1;
     }
-
-    ShowWindow(hWnd, SW_SHOWNORMAL);
-    UpdateWindow(hWnd);
 
     NOTIFYICONDATA nif;
     // タスクトレイに登録
@@ -260,5 +258,51 @@ void AppicationInit(HWND hWnd) {
     menu.CreateMenu(hWnd);
     timer.StartTimer(hWnd, 0, minute, TimerMenuItemLabelChange);
     menu.Command(hWnd, 1, &timer);
+
+    //resister startup application
+    wchar_t szPathToExe[MAX_PATH];
+    GetModuleFileNameW(NULL, szPathToExe, MAX_PATH);
+    RegisterMyProgramForStartup(szWindowClass, szPathToExe, NULL);
 }
 
+BOOL RegisterMyProgramForStartup(PCWSTR pszAppName, PCWSTR pathToExe, PCWSTR args)
+{
+    HKEY hKey = NULL;
+    LONG lResult = 0;
+    BOOL fSuccess = TRUE;
+    DWORD dwSize;
+
+    const size_t count = MAX_PATH * 2;
+    wchar_t szValue[count] = {};
+
+
+    wcscpy_s(szValue, count, L"\"");
+    wcscat_s(szValue, count, pathToExe);
+    wcscat_s(szValue, count, L"\" ");
+
+    if (args != NULL)
+    {
+        // caller should make sure "args" is quoted if any single argument has a space
+        // e.g. (L"-name \"Mark Voidale\"");
+        wcscat_s(szValue, count, args);
+    }
+
+    lResult = RegCreateKeyExW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, NULL, 0, (KEY_WRITE | KEY_READ), NULL, &hKey, NULL);
+
+    fSuccess = (lResult == 0);
+
+    if (fSuccess)
+    {
+        dwSize = (wcslen(szValue) + 1) * 2;
+        lResult = RegSetValueExW(hKey, pszAppName, 0, REG_SZ, (BYTE*)szValue, dwSize);
+        fSuccess = (lResult == 0);
+    }
+
+    if (hKey != NULL)
+    {
+        RegCloseKey(hKey);
+        hKey = NULL;
+    }
+
+    return fSuccess;
+}
